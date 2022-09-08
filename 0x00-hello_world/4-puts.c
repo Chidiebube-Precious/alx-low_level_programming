532c7364a769f0ceeb5f02bf446f59b41921fdac
Chidiebube Precious <precious.ezeokoye.235952@unn.edu.ng>
2022-09-08 22:51:02 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
-* main - main block
-* Return: 0
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+char test_str[] = "\"Programming is like building a multilingual puzzle";
+puts(test_str);
 return (0);
 }
