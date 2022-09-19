04219f8647518d60c3860f7e4b9135d83ee12f74
Chidiebube Precious <precious.ezeokoye.235952@unn.edu.ng>
2022-09-19 21:59:59 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @S: this is the input string
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *S)
+{
+	int index;
+
+	for (index = 0; S[index] != '\0' ; index++)
+
+	return (index);
+}
