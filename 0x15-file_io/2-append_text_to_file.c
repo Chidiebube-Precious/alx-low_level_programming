e0894f1498ca8d9991138b06f466fef08395441f
Chidiebube-Precious <precious.ezeokoye.235952@unn.edu.ng>
2022-11-14 13:56:50 +0000

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	file = open(filename, 0_WRONGLY | 0_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 		return (-1);
